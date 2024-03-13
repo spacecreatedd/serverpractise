a2db2aa7b9156dd19916d2959b953756ce6c7a88
spacecreatedd <kimzhenya1313@mail.ru>
2024-03-13 21:55:37 +0700

    bootstrap and path editing

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'spacecreatedd/pop-it-mvc',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f0ecbbc19e98ab380d5be3dc7a14c4bcd41af275',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'spacecreatedd/pop-it-mvc' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f0ecbbc19e98ab380d5be3dc7a14c4bcd41af275',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

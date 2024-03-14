0a99e14cf606e7f3f725e5926d08885ad65405ae
spacecreatedd <kimzhenya1313@mail.ru>
2024-03-14 23:08:07 +0700

    login and register function

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit6f6fdd74bb80402e1110bdeb8652006a
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit6f6fdd74bb80402e1110bdeb8652006a::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
